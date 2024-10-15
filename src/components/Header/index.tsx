598af42ebfb60374ca52eb175c213aef3b5c7c8d
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-10-15 12:03:31 -0300

    feat: add Dialog component from Radix UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
 
 export function Header() {
@@ -7,7 +8,23 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="Logo do DT Money" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        {/* dialog para abrir o modal ao clicar no botao */}
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                
+                <Dialog.Close />
+              </Dialog.Content>
+          </Dialog.Portal>
+
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   )
